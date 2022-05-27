d679b7d734c33f0572322c0c57c1daabf7a1d2b2
Geovane Souza <devel_20@autoavaliar.com.br>
2022-05-27 17:44:56 -0300

    Fist commit 2

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=0, shrink-to-fit=no" />
+        <meta name="description" content="Eletrex serviços elétricos" />
+        <meta name="author" content="Geovane Souza" />
+        <title>Eletrex - Serviços Elétricos</title>
+        <!-- Favicon-->
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Bootstrap Icons-->
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <!-- SimpleLightbox plugin CSS-->
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.css" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark fixed-top py-3" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">
+                    <img class="img-fluid" src="./assets/img/logo.png" />
+                </a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link" href="#about">Sobre nós</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#services">Serviços</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#portfolio">Portfolio</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#contact">Contato</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#" onclick="window.open('./assets/apresentacao.pdf', '_blank', 'fullscreen=yes'); return false;">Apresentação</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#location">Localização</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 h-100">
+                <div class="row gx-4 gx-lg-5 h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-8 align-self-end">
+                        <h1 class="text-white font-weight-bold">Eletrex, um eletricista sempre perto de você</h1>
+                        <hr class="divider" />
+                    </div>
+                    <div class="col-lg-8 align-self-baseline">
+                        <p class="text-white-75 mb-5">A Eletrex atua com serviços elétricos em toda região de São Paulo, sempre seguindo os padrões de seguraça e respeitando os seus clientes.</p>
+                        <a class="btn btn-primary btn-xl" href="#about">Leia mais</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About-->
+        <section class="page-section bg-primary" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="text-white mt-0">Quem somos?</h2>
+                        <hr class="divider divider-light" />
+                        <p class="text-white-75 mb-4">Composta por profissionais com anos de experiência
+                            na área de eletricidade, exercemos atividades
+                            simples, desde a troca de tomadas e também
+                            complexas, como a instalação elétrica completa de
+                            prédios e empreendimentos.
+                            O foco da Eletrex é atuar na área sempre com um
+                            alto padrão de trabalho pelo melhor valor do
+                            mercado.</p>
+                        <a class="btn btn-light btn-xl" href="#services">Veja nossos serviços!</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Services-->
+        <section class="page-section" id="services">
+            <div class="container px-4 px-lg-5">
+                <h2 class="text-center mt-0">Nossos serviços</h2>
+                <hr class="divider" />
+                <div class="row gx-4 gx-lg-5">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-calendar2-range fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Quadros elétricos</h3>
+                            <p class="text-muted mb-0">A Eletrex é especializada em montagem e manuteção de diversos tipos de quados elétricos.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-lightning-charge fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Manuteção</h3>
+                            <p class="text-muted mb-0">Fazemos manuteção preventiva da sua rede interna e externa.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-lightbulb-off fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Conserto</h3>
+                            <p class="text-muted mb-0">Temos experiência em rosolver diversos tipo de problemas causados na sua rede elétrica.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <div class="mb-2"><i class="bi-brightness-high fs-1 text-primary"></i></div>
+                            <h3 class="h4 mb-2">Instalação</h3>
+                            <p class="text-muted mb-0">Realizamos instações de diversos tipos, seja residência ou comercial.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Portfolio-->
+        <div id="portfolio">
+            <div class="container-fluid p-0">
+                <div class="row g-0">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/quadro-eletrico.jpg" title="Quadro elétrico">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/quadro-eletrico.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-name">Quadro Elétrico</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/manutencao-predial.mp4" title="Instalação Predial">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/manutencao-predial.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-name">Instalação Predial</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/3.jpg" title="Project Name">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/3.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Category</div>
+                                <div class="project-name">Project Name</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/4.jpg" title="Project Name">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/4.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Category</div>
+                                <div class="project-name">Project Name</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/5.jpg" title="Project Name">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/5.jpg" alt="..." />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Category</div>
+                                <div class="project-name">Project Name</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="assets/img/portfolio/fullsize/6.jpg" title="Project Name">
+                            <img class="img-fluid" src="assets/img/portfolio/thumbnails/6.jpg" alt="..." />
+                            <div class="portfolio-box-caption p-3">
+                                <div class="project-category text-white-50">Category</div>
+                                <div class="project-name">Project Name</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Call to action-->
+        <section class="page-section bg-dark text-white">
+            <div class="container px-4 px-lg-5 text-center">
+                <h2 class="mb-4">Faça um orçamento conosco, é totalmente grátis!</h2>
+                <a class="btn btn-light btn-xl" href="#contact">Fazer um orçamento</a>
+            </div>
+        </section>
+        <!-- Contact-->
+        <section class="page-section" id="contact">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 col-xl-6 text-center">
+                        <h2 class="mt-0">Vamos fazer um orçamento!</h2>
+                        <hr class="divider" />
+                        <p class="text-muted mb-5">Estamos à disposição para ouvir o seu problema e te atender da melhor forma possível, nós disponibilizamos dois canais de atendimento para você, <b class="text-primary">Email</b>
+                             e <b class="text-primary">WhatsApp</b>, fique a vontade para falar conosco.</p>
+                    </div>
+                </div>
+                <!-- <div class="row gx-4 gx-lg-5 justify-content-center mb-5">
+                    <div class="col-lg-6"> -->
+                        
+                        <!-- <form id="contactForm" data-sb-form-api-token="API_TOKEN">
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="name" type="text" placeholder="Enter your name..." data-sb-validations="required" />
+                                <label for="name">Full name</label>
+                                <div class="invalid-feedback" data-sb-feedback="name:required">A name is required.</div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="email" type="email" placeholder="name@example.com" data-sb-validations="required,email" />
+                                <label for="email">Email address</label>
+                                <div class="invalid-feedback" data-sb-feedback="email:required">An email is required.</div>
+                                <div class="invalid-feedback" data-sb-feedback="email:email">Email is not valid.</div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <input class="form-control" id="phone" type="tel" placeholder="(123) 456-7890" data-sb-validations="required" />
+                                <label for="phone">Phone number</label>
+                                <div class="invalid-feedback" data-sb-feedback="phone:required">A phone number is required.</div>
+                            </div>
+                            <div class="form-floating mb-3">
+                                <textarea class="form-control" id="message" type="text" placeholder="Enter your message here..." style="height: 10rem" data-sb-validations="required"></textarea>
+                                <label for="message">Message</label>
+                                <div class="invalid-feedback" data-sb-feedback="message:required">A message is required.</div>
+                            </div>
+                            <div class="d-none" id="submitSuccessMessage">
+                                <div class="text-center mb-3">
+                                    <div class="fw-bolder">Form submission successful!</div>
+                                    To activate this form, sign up at
+                                    <br />
+                                    <a href="https://startbootstrap.com/solution/contact-forms">https://startbootstrap.com/solution/contact-forms</a>
+                                </div>
+                            </div>
+                            <div class="d-none" id="submitErrorMessage"><div class="text-center text-danger mb-3">Error sending message!</div></div>
+                            <div class="d-grid"><button class="btn btn-primary btn-xl disabled" id="submitButton" type="submit">Submit</button></div>
+                        </form> -->
+                    <!-- </div>
+                </div> -->
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-4 text-center">
+                        <i class="bi-whatsapp fs-2 mb-3 text-muted"></i>
+                        <div>
+                            <a href="https://wa.me/5511979584179" target="_blank">+55 (11) 97958-4179</a>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="row gx-4 gx-lg-5 justify-content-center mt-4">
+                    <div class="col-lg-4 text-center">
+                        <i class="bi-inbox fs-2 mb-3 text-muted"></i>
+                        <div>
+                            <a target="_blank" href="mailto:orcamento@eletrex.com.br">Enviar email</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Maps -->
+        <section id="location">
+            <div class="row gx-4 gx-lg-5 justify-content-center">
+                <div class="col-lg-8 col-xl-6 text-center">
+                    <h2 class="mt-0">Onde estamos?</h2>
+                    <hr class="divider" />
+                </div>
+            </div>
+            <div class="map" id="contact">
+                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3655.092153259345!2d-46.741720585413226!3d-23.63687058464546!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x94ce515b604d3d61%3A0x1423846d08e8dd4b!2sR.%20Alexandre%20Archipenko%2C%20225%20-%20Vila%20Andrade%2C%20S%C3%A3o%20Paulo%20-%20SP%2C%2005729-080!5e0!3m2!1spt-BR!2sbr!4v1653677812676!5m2!1spt-BR!2sbr" style="border:0;" allowfullscreen loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+            </div>
+        </section>
+
+        <!-- Footer-->
+        <footer class="bg-light py-5">
+            <div class="container px-4 px-lg-5"><div class="small text-center text-muted">&copy; 2022 - Eletrex, Serviços Elétricos</div></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- SimpleLightbox plugin JS-->
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/SimpleLightbox/2.1.0/simpleLightbox.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <script src="js/config.js"></script>
+    </body>
+</html>
